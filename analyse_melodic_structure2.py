86ff0e126b215d809cb698147b2e13aa5387d2d8
Rory Sweeney <r.sweeney4@universityofgalway.ie>
2024-06-26 21:10:09 +0100

    Moved analysis code to a function and added functionality to process all files in a directory. Filenames included in output.

--- analyse_melodic_structure2.py
+++ analyse_melodic_structure2.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import collections
+import sys
+import os
+from tqdm import tqdm
 import pprint
-import re
 
 def load_data(file_path):
     # Load the CSV file into a DataFrame
@@ -22,115 +24,135 @@ def load_data(file_path):
 
     return parts
 
-# Path to the CSV file
-file_path = '/home/roro/Documents/RA2/code/melodic_struc_analysis/feature_CSVs/duration_weighted/Shandonbells.csv'
+def analyse_tune(in_path, filename, outputfile):
+    # Path to the CSV file
+    #file_path = '/home/roro/Documents/RA2/code/melodic_struc_analysis/feature_CSVs/duration_weighted/Shandonbells.csv'
+    tunename = os.path.splitext(filename)[0]
 
-# Load the data into the desired structure
-parts = load_data(file_path)
+    # Load the data into the desired structure
+    parts = load_data(f"{in_path}/{filename}")
 
-part_patterns = {}
-tune_patterns = {}
+    part_patterns = {}
+    tune_patterns = {}
 
-curr_part_letter = 'A'
+    curr_part_letter = 'A'
 
-for part_num, bars in parts.items():
-    curr_letter = 'a'
-    part_label = ""
-    if part_num not in part_patterns:
-        part_patterns[part_num] = {}
+    for part_num, bars in parts.items():
+        curr_letter = 'a'
+        part_label = ""
+        if part_num not in part_patterns:
+            part_patterns[part_num] = {}
 
-    for bar_num, notes in bars.items():
-        v1 = [note['midi_note_num'] for note in notes]
+        for bar_num, notes in bars.items():
+            v1 = [note['midi_note_num'] for note in notes]
 
-        # Check if there are any previous patterns stored.
+            # Check if there are any previous patterns stored.
+            found_match = False
+            # Loop backwards from current part to the first part.
+            for prev_part_num in reversed(part_patterns):
+                # Is this needed?
+                if prev_part_num not in part_patterns:
+                    continue
+                letter_prefix = '' if part_num == prev_part_num else tune_patterns[prev_part_num]['letter']
+
+                # Loop over the bars in the previous part.
+                for prev_bar_num, prev_bar in part_patterns[prev_part_num].items():
+                    prev_notes = prev_bar['notes']
+
+                    diff = [xi - yi for xi, yi in zip(v1, prev_notes)]
+                    comparison = collections.Counter(diff).most_common(1)[0][1]
+
+                    # Compare the current bar with previous bars for commonality.
+                    if comparison >= 5:
+                        # Identical or near-identical to previous pattern.
+                        part_patterns[part_num][bar_num] = {'notes': v1, 'letter': part_patterns[prev_part_num][prev_bar_num]['letter'], 'suffix': "", 'prefix': letter_prefix, 'variant_counter': 0}
+                        found_match = True
+                        break
+
+                    elif comparison >= 3 and comparison < 5:
+                        # Variant of a previous pattern.
+
+                        # Exclude matches with other variants.
+                        if part_patterns[prev_part_num][prev_bar_num]['suffix'] != "":
+                            continue
+
+                        # Determine suffix number with variant counter.
+                        part_patterns[prev_part_num][prev_bar_num]['variant_counter'] += 1
+                        suffix = str(part_patterns[prev_part_num][prev_bar_num]['variant_counter'])
+                        part_patterns[part_num][bar_num] = {'notes': v1, 'letter': part_patterns[prev_part_num][prev_bar_num]['letter'], 'suffix': suffix, 'prefix': letter_prefix, 'variant_counter': 0}
+                        found_match = True
+                        break
+                if found_match:
+                    break
+            if not found_match:
+                # New unique pattern.
+                part_patterns[part_num][bar_num] = {'notes': v1, 'letter': curr_letter, 'suffix': "", 'prefix': "", 'variant_counter': 0}
+                curr_letter = chr(ord(curr_letter) + 1)
+
+            part_label += part_patterns[part_num][bar_num]['prefix'] + part_patterns[part_num][bar_num]['letter'] + part_patterns[part_num][bar_num]['suffix']
+
+        tune_patterns[part_num] = {}
+        tune_patterns[part_num]['pattern'] = part_label
+        tune_patterns[part_num]['variant_counter'] = 0
+        # Check if the current part label is the same as any previous ones.
+        # Need to remove numbers?
+
         found_match = False
-        # Loop backwards from current part to the first part.
-        for prev_part_num in reversed(part_patterns):
-            # Is this needed?
-            if prev_part_num not in part_patterns:
+        # Loop over previous parts in tune.
+        for prev_part_num in part_patterns:
+            # Skip the current part. Check that this is reference equality.
+            if prev_part_num == part_num:
                 continue
-            letter_prefix = '' if part_num == prev_part_num else tune_patterns[prev_part_num]['letter']
+            # Counter for the number of bars in common between the two parts.
+            bar_counter = 0
+            # Loop over bars in previous part.
+            for prev_part_bar, curr_part_bar in zip(part_patterns[prev_part_num].values(), part_patterns[part_num].values()):
+                # Count the number of identical bars.
+                if prev_part_bar['letter'] == curr_part_bar['letter']:
+                    bar_counter += 1
 
-            # Loop over the bars in the previous part.
-            for prev_bar_num, prev_bar in part_patterns[prev_part_num].items():
-                prev_notes = prev_bar['notes']
-
-                diff = [xi - yi for xi, yi in zip(v1, prev_notes)]
-                comparison = collections.Counter(diff).most_common(1)[0][1]
-
-                # Compare the current bar with previous bars for commonality.
-                if comparison >= 5:
-                    # Identical or near-identical to previous pattern.
-                    part_patterns[part_num][bar_num] = {'notes': v1, 'letter': part_patterns[prev_part_num][prev_bar_num]['letter'], 'suffix': "", 'prefix': letter_prefix, 'variant_counter': 0}
-                    found_match = True
-                    break
-
-                elif comparison >= 3 and comparison < 5:
-                    # Variant of a previous pattern.
-
-                    # Exclude matches with other variants.
-                    if part_patterns[prev_part_num][prev_bar_num]['suffix'] != "":
-                        continue
-
-                    # Determine suffix number with variant counter.
-                    part_patterns[prev_part_num][prev_bar_num]['variant_counter'] += 1
-                    suffix = str(part_patterns[prev_part_num][prev_bar_num]['variant_counter'])
-                    part_patterns[part_num][bar_num] = {'notes': v1, 'letter': part_patterns[prev_part_num][prev_bar_num]['letter'], 'suffix': suffix, 'prefix': letter_prefix, 'variant_counter': 0}
-                    found_match = True
-                    break
-            if found_match:
+            #if bar_counter == 8:
+            #    tune_patterns[part_num]['letter'] = tune_patterns[prev_part_num]['letter']
+            #    # Change to counter.
+            #    tune_patterns[part_num]['suffix'] = " "
+            #    found_match = True
+            #    break
+            if bar_counter >= 6:
+                tune_patterns[part_num]['letter'] = tune_patterns[prev_part_num]['letter']
+                tune_patterns[prev_part_num]['variant_counter'] += 1
+                tune_patterns[part_num]['suffix'] = str(tune_patterns[prev_part_num]['variant_counter'])
+                found_match = True
                 break
         if not found_match:
-            # New unique pattern.
-            part_patterns[part_num][bar_num] = {'notes': v1, 'letter': curr_letter, 'suffix': "", 'prefix': "", 'variant_counter': 0}
-            curr_letter = chr(ord(curr_letter) + 1)
+            tune_patterns[part_num]['letter'] = curr_part_letter
+            curr_part_letter = chr(ord(curr_part_letter) + 1)
+            tune_patterns[part_num]['suffix'] = " "
 
-        part_label += part_patterns[part_num][bar_num]['prefix'] + part_patterns[part_num][bar_num]['letter'] + part_patterns[part_num][bar_num]['suffix']
+    # Return the melodic structure patterns to the calling function.
+    output = []
+    for part, tune_label in zip(part_patterns.values(), tune_patterns.values()):
+        part_label = tune_label['letter'] + tune_label['suffix'] + ": "
+        for bar in part.values():
+            if tune_label['letter'] == bar['prefix']:
+                part_label += bar['letter'] + bar['suffix']
+            else:
+                part_label += bar['prefix'] + bar['letter'] + bar['suffix']
+        output.append(tunename + ", \"" + part_label + "\"\n")
 
-    tune_patterns[part_num] = {}
-    tune_patterns[part_num]['pattern'] = part_label
-    tune_patterns[part_num]['variant_counter'] = 0
-    # Check if the current part label is the same as any previous ones.
-    # Need to remove numbers?
+    outputfile.writelines(output)
 
-    found_match = False
-    # Loop over previous parts in tune.
-    for prev_part_num in part_patterns:
-        # Skip the current part. Check that this is reference equality.
-        if prev_part_num == part_num:
-            continue
-        # Counter for the number of bars in common between the two parts.
-        bar_counter = 0
-        # Loop over bars in previous part.
-        for prev_part_bar, curr_part_bar in zip(part_patterns[prev_part_num].values(), part_patterns[part_num].values()):
-            # Count the number of identical bars.
-            if prev_part_bar['letter'] == curr_part_bar['letter']:
-                bar_counter += 1
+def analyse_melodic_structure(in_path):
+    outputfile = open("melodic_structures.txt", "w")
+    filetypes = ('.csv')
 
-        #if bar_counter == 8:
-        #    tune_patterns[part_num]['letter'] = tune_patterns[prev_part_num]['letter']
-        #    # Change to counter.
-        #    tune_patterns[part_num]['suffix'] = " "
-        #    found_match = True
-        #    break
-        if bar_counter >= 6:
-            tune_patterns[part_num]['letter'] = tune_patterns[prev_part_num]['letter']
-            tune_patterns[prev_part_num]['variant_counter'] += 1
-            tune_patterns[part_num]['suffix'] = str(tune_patterns[prev_part_num]['variant_counter'])
-            found_match = True
-            break
-    if not found_match:
-        tune_patterns[part_num]['letter'] = curr_part_letter
-        curr_part_letter = chr(ord(curr_part_letter) + 1)
-        tune_patterns[part_num]['suffix'] = " "
+    print(in_path)
 
+    if os.path.isdir(in_path):
+        filenames = [file for file in os.listdir(in_path) if file.endswith(filetypes)]
+        #print(filenames)
+        for file in tqdm(filenames, desc='Analysing Melodic Structure.'):
+            analyse_tune(in_path, file, outputfile)
 
-# Output the melodic structure patterns to the console.
-for part, tune_label in zip(part_patterns.values(), tune_patterns.values()):
-    part_label = tune_label['letter'] + tune_label['suffix'] + ": "
-    for bar in part.values():
-        if tune_label['letter'] == bar['prefix']:
-            part_label += bar['letter'] + bar['suffix']
-        else:
-            part_label += bar['prefix'] + bar['letter'] + bar['suffix']
-    print(part_label)
+if __name__ == "__main__":
+    in_path = sys.argv[1]
+    analyse_melodic_structure(in_path)
