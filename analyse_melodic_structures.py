654aebef191e5321571e056d89a2fda35d33e6dc
Rory Sweeney <rory.sweeney@universityofgalway.ie>
2024-07-23 23:24:42 +0100

    Added improved part-labelling code. Removed duplicate parts.

--- analyse_melodic_structures.py
+++ analyse_melodic_structures.py
@@ -46,6 +46,7 @@ def extract_tune_notes(score):
     eighth_notes_per_bar = float(num) * 8 / float(den)
 
     notes = []
+    measure_nums = []
     bar_count = 0
     skip_next = False
     for i in range(len(measures)):
@@ -76,12 +77,54 @@ def extract_tune_notes(score):
         part_count = bar_count // 8
         if bar_count % 8 == 0:
             notes.append([])
+            measure_nums.append([])
         notes[part_count].append(bar_notes)
+        measure_nums[part_count].append(measure.number)
         bar_count += 1
-    return notes
+
+    # Part labelling
+    curr_letter = 'A'
+    non_variant_part = []
+    variant_counter = []
+    part_labels = []
+    removal_list = []
+    for part_num in range(len(measure_nums)):
+        part_labels.append([])
+        non_variant_part.append(False)
+        variant_counter.append(1)
+        score = 0
+        match_found = False
+        for prev_part_num in range(part_num):
+            diff = [xi - yi for xi, yi in zip(measure_nums[part_num], measure_nums[prev_part_num])]
+            num_common = diff.count(0)
+            # Full match.
+            if num_common == 8:
+                part_labels[part_num] = part_labels[prev_part_num]
+                match_found = True
+                removal_list.append(part_num)
+                break
+            # Partial match.
+            elif num_common >= 6:
+                if num_common > score and non_variant_part[prev_part_num]:
+                    part_labels[part_num] = part_labels[prev_part_num] + str(variant_counter[prev_part_num])
+                    variant_counter[prev_part_num] += 1
+                    score = num_common
+                    match_found = True
+        # New part letter.
+        if not match_found:
+            part_labels[part_num] = curr_letter
+            curr_letter = chr(ord(curr_letter) + 1)
+            non_variant_part[part_num] = True
+    # Remove duplicate parts.
+    for i in reversed(removal_list):
+        notes.pop(i)
+        part_labels.pop(i)
+    return notes, part_labels
 
 
-def analyse_tune(tune_notes, tune_name, tune_number):
+# Function to generate Doherty melodic structures for each part in a passed tune represented as a nested list
+# of MIDI notes.
+def analyse_tune(tune_notes, tune_name, tune_number, part_labels):
     delimiter_options = {0: "",
                          1: ", ",
                          2: "",
@@ -90,19 +133,12 @@ def analyse_tune(tune_notes, tune_name, tune_number):
                          5: ", ",
                          6: "",
                          7: "."}
-
     part_patterns = {}
-    tune_patterns = {}
-
-    curr_part_letter = 'A'
-
     part_num = 0
     for part in tune_notes:
         curr_letter = 'a'
-        part_label = ""
         if part_num not in part_patterns:
             part_patterns[part_num] = {}
-
         bar_num = 0
         full_match_score = 0
         partial_match_score = 0
@@ -111,38 +147,20 @@ def analyse_tune(tune_notes, tune_name, tune_number):
             temp_bar_pattern = {}
             full_match = False
             partial_match = False
-            # Check if there are any previous patterns stored.
             # Loop backwards from current part to the first part.
-            for prev_part_num in reversed(part_patterns):
-                letter_prefix = '' if part_num == prev_part_num else tune_patterns[prev_part_num]['letter']
-
-                # Loop over the bars in the previous part.
+            for prev_part_num in part_patterns:
+                letter_prefix = '' if part_num == prev_part_num else part_labels[prev_part_num]
+                # Loop over the bars in the previous parts and compare for commonality.
                 for prev_bar_num, prev_bar in part_patterns[prev_part_num].items():
                     prev_notes = prev_bar['notes']
-
                     diff = [xi - yi for xi, yi in zip(bar, prev_notes)]
-
                     # For full match (without transposition)
                     num_common = diff.count(0)/max(len(bar), len(prev_notes))
-
                     # For partial match (transposition allowed)
                     most_common_delta = collections.Counter(diff).most_common(1)[0]
                     comparison = most_common_delta[1]/max(len(bar), len(prev_notes))
-
-                    # Identify fully transposed bars.
-                    #if comparison == 1 and most_common_delta[0] != 0:
-                    #    print(str(part_num + 1) + " " + str(bar_num + 1) + "\n")
-                    #    print(bar)
-                    #    print("\n")
-                    #    print(str(prev_part_num + 1) + " " + str(prev_bar_num + 1) + "\n")
-                    #    print(prev_notes)
-                    #    print("\n")
-                    #    print(diff)
-                    #    print("\n")
-
-                    # Compare the current bar with previous bars for commonality.
+                    # Identical or near-identical to previous pattern.
                     if num_common >= 5/6:
-                        # Identical or near-identical to previous pattern.
                         temp_bar_pattern = {'notes': bar,
                                             'letter': part_patterns[prev_part_num][prev_bar_num]['letter'],
                                             'suffix': part_patterns[prev_part_num][prev_bar_num]['suffix'],
@@ -150,27 +168,22 @@ def analyse_tune(tune_notes, tune_name, tune_number):
                                             'prefix': letter_prefix,
                                             'variant_counter': 0}
                         full_match = True
-
                         # Calculate the full match score and compare.
                         if num_common <= full_match_score:
                             continue
                         else:
                             full_match_score = num_common
-
+                    # Variant of a previous pattern.
                     elif 3/6 <= comparison < 5/6:
-                        # Variant of a previous pattern.
-
                         # Exclude matches with other variants.
                         if part_patterns[prev_part_num][prev_bar_num]['suffix'] != "":
                             continue
-
                         # Calculate the partial match score and compare.
                         score = comparison / (abs(most_common_delta[0]) + 1)
                         if score <= partial_match_score:
                             continue
                         else:
                             partial_match_score = score
-
                         # Determine suffix number with variant counter.
                         part_patterns[prev_part_num][prev_bar_num]['variant_counter'] += 1
                         suffix = str(part_patterns[prev_part_num][prev_bar_num]['variant_counter'])
@@ -192,81 +205,39 @@ def analyse_tune(tune_notes, tune_name, tune_number):
                 curr_letter = chr(ord(curr_letter) + 1)
             else:
                 part_patterns[part_num][bar_num] = temp_bar_pattern
-
-            part_label += (part_patterns[part_num][bar_num]['prefix'] + part_patterns[part_num][bar_num]['letter'] +
-                           part_patterns[part_num][bar_num]['suffix'])
             bar_num += 1
-
-        tune_patterns[part_num] = {}
-        tune_patterns[part_num]['pattern'] = part_label
-        tune_patterns[part_num]['variant_counter'] = 0
-        # Check if the current part label is the same as any previous ones.
-
-        ### Part labelling:
-        found_match = False
-        # Loop over previous parts in tune.
-        for prev_part_num in part_patterns:
-            # Skip the current part. Check that this is reference equality.
-            if prev_part_num == part_num:
-                continue
-            # Counter for the number of bars in common between the two parts.
-            bar_counter = 0
-            # Loop over bars in previous part.
-            for prev_part_bar, curr_part_bar in zip(part_patterns[prev_part_num].values(),
-                                                    part_patterns[part_num].values()):
-                # Count the number of identical bars.
-                if prev_part_bar['letter'] == curr_part_bar['letter']:
-                    bar_counter += 1
-
-            if bar_counter >= 6:
-                tune_patterns[part_num]['letter'] = tune_patterns[prev_part_num]['letter']
-                tune_patterns[prev_part_num]['variant_counter'] += 1
-                tune_patterns[part_num]['suffix'] = str(tune_patterns[prev_part_num]['variant_counter'])
-                found_match = True
-                break
-        if not found_match:
-            tune_patterns[part_num]['letter'] = curr_part_letter
-            curr_part_letter = chr(ord(curr_part_letter) + 1)
-            tune_patterns[part_num]['suffix'] = ""
-
         part_num += 1
-
     # Output the melodic structure patterns to the output file.
     output = []
-    for part, tune_label in zip(part_patterns.values(), tune_patterns.values()):
-        part_label = tune_label['letter'] + tune_label['suffix']
+    for part, tune_label in zip(part_patterns.values(), part_labels):
         structure = ""
         for bar in part.values():
-            if tune_label['letter'] == bar['prefix']:
+            if tune_label[0] == bar['prefix']:
                 structure += bar['letter'] + bar['suffix'] + bar['delimiter']
             else:
                 structure += bar['prefix'] + bar['letter'] + bar['suffix'] + bar['delimiter']
-        output.append(tune_number + "," + tune_name + "," + part_label + ",\"" + structure + "\"\n")
-
+        output.append(tune_number + "," + tune_name + "," + tune_label + ",\"" + structure + "\"\n")
     return output
 
 
+# Function to extract the tune number and title from its ABC representation.
 def extract_abc_info(abc_string):
     lines = abc_string.split('\n')
     title = None
     number = None
-
     for line in lines:
         if line.startswith('X:'):
             number = line[2:].strip()
         elif line.startswith('T:'):
             title = line[2:].strip()
-
     return title, number
 
 
 def remove_macros(abc_string):
     # Split the input into lines
     lines = abc_string.split('\n')
-
     # To store the uppercase letters to be removed and the processed lines
     letters_to_remove = set()
-
     # Find macro flags to remove.
     for line in lines:
         if line.startswith('m:'):
@@ -275,7 +246,6 @@ def remove_macros(abc_string):
             if match:
                 letter = match.group(1)
                 letters_to_remove.add(letter)
-
     # Remove all instances of the letters to be removed from the processed string
     processed_lines = []
     for line in lines:
@@ -286,14 +256,12 @@ def remove_macros(abc_string):
         else:
             processed_lines.append(line)
     processed_string = '\n'.join(processed_lines)
-
     lines = processed_string.split('\n')
     processed_lines = []
     for line in lines:
         if not line.startswith('m:'):
             processed_lines.append(line)
     processed_string = '\n'.join(processed_lines)
-
     return processed_string
 
 
@@ -309,7 +277,6 @@ def clean_abc(abc_content):
         else:
             processed_lines.append(line)
     processed_string = '\n'.join(processed_lines)
-
     return processed_string
 
 
@@ -319,22 +286,18 @@ def process_tune(abc_content):
     # Substitute repeat shorthand.
     abc_content = abc_content.replace("::", ":||:")
     abc_content = clean_abc(abc_content)
-
     # Parse the ABC content
     abc_score = converter.parse(abc_content, format='abc')
     # Expand repeats
     expanded_score = abc_score.expandRepeats()
-
     tune_name, tune_number = extract_abc_info(abc_content)
     # Generate a list of lists containing the notes in each bar as MIDI numbers.
-    tune_notes = extract_tune_notes(expanded_score)
-
+    tune_notes, part_labels = extract_tune_notes(expanded_score)
     #print(tune_number + " " + tune_name + "\n")
     # if tune_number == '6':
     #    pprint.pp(tune_notes)
-
     # Generate Doherty structure pattern strings.
-    return analyse_tune(tune_notes, tune_name, tune_number)
+    return analyse_tune(tune_notes, tune_name, tune_number, part_labels)
 
 
 # Function to read the contents of an abc file, strip any header material that does not form part of a description of a
@@ -342,29 +305,22 @@ def process_tune(abc_content):
 def read_abc_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
-
     # Split the content into lines
     lines = content.split('\n')
-
     # List of common metadata fields in .abc files
     metadata_fields = {'X:', 'T:', 'M:', 'K:', 'L:', 'Q:', 'C:', 'R:', 'N:', 'P:'}
-
     # Identify the end of the header section
     header_end_index = 0
     for i, line in enumerate(lines):
         if line.strip()[:2] in metadata_fields:
             header_end_index = i
             break
-
     # The tunes start at the header end index
     tunes_content = '\n'.join(lines[header_end_index:])
-
     # Split the remaining content into tunes based on double newlines (indicating a blank line)
     tunes = tunes_content.strip().split('\n\n')
-
     # Clean up any extra newlines or leading/trailing spaces in each tune
     tunes = [tune.strip() for tune in tunes if tune.strip()]
-
     return tunes
 
 
